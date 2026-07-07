93bf081b1b34e845dbba5e922e1f77b9746a8172
Rijan Bhattarai <rijanbhattarai2006@gmail.com>
2026-07-07 16:56:57 +0545

    Feat: Runner add

--- src/com/interpreters/rorix/Rorix.java
+++ src/com/interpreters/rorix/Rorix.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Rorix {
     public static void main(String[] args) throws IOException{
@@ -24,6 +25,26 @@ public class Rorix {
     public static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
-        return ;
+    }
+
+    public static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.println("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token: tokens) {
+            System.out.println(token);
+        }
     }
 }
