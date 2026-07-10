8aef3eb56a2d30082fddbedaf3c3b8097c8d91ce
Rijan Bhattarai <rijanbhattarai2006@gmail.com>
2026-07-10 07:23:22 +0545

    Fix: Add + Operator, Add {} Braces, Add \n character, Improve Formatting

--- src/com/interpreters/rorix/Scanner.java
+++ src/com/interpreters/rorix/Scanner.java
@@ -9,10 +9,8 @@ import static com.interpreters.rorix.TokenType.*;
 
 
 public class Scanner {
-
     private final String source;
-    private final List<Token> tokens = new ArrayList<>();
-
+    private List<Token> tokens = new ArrayList<>();
     private int start = 0;
     private int current = 0;
     private int line = 1;
@@ -36,6 +34,7 @@ public class Scanner {
         keywords.put("true", TRUE);
         keywords.put("var", VAR);
         keywords.put("while", WHILE);
+        keywords.put("fun", FUN);
     }
 
     Scanner(String source) {
@@ -47,7 +46,6 @@ public class Scanner {
             start = current;
             scanToken();
         }
-
         tokens.add(new Token(EOF, "", null, line));
         return tokens;
     }
@@ -56,17 +54,18 @@ public class Scanner {
         return current >= source.length();
     }
 
-    private void scanToken() {
+    public void scanToken() {
         char c = advance();
 
         switch (c) {
             case '(': addToken(LEFT_PAREN); break;
             case ')': addToken(RIGHT_PAREN); break;
-            case '[': addToken(LEFT_BRACE); break;
-            case ']': addToken(RIGHT_BRACE); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
             case ',': addToken(COMMA); break;
             case '.': addToken(DOT); break;
             case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
 
@@ -83,36 +82,37 @@ public class Scanner {
             case '>':
                 addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
+
+            // Filter out comments
             case '/':
-                // A comment goes until the end of line
                 if (match('/')) {
+                    // Skip the entire line if it is a comment
                     while (peek() != '\n' && !isAtEnd()) advance();
                 }
                 else {
                     addToken(SLASH);
                 }
                 break;
+
+            // Skip Whitespaces
             case ' ':
             case '\r':
             case '\t':
                 break;
+
+            // Handle New Lines
             case '\n':
                 line++;
                 break;
-            case '"': string(); break;
 
-            // Reserved Keywords and Identifiers
-            case 'o':
-                if (match('r')) {
-                    addToken(OR);
-                }
-                break;
+            // Identify Literals
+            case '"': string(); break;
 
             default:
                 if (isDigit(c)) {
                     number();
                 }
-                else if (isAlpha(c)) {
+                else if (isAlphaNumeric(c)) {
                     identifier();
                 }
                 else {
@@ -131,15 +131,6 @@ public class Scanner {
         addToken(type);
     }
 
-    private void addToken(TokenType type) {
-        addToken(type, null);
-    }
-
-    private void addToken(TokenType type, Object literal) {
-        String text = source.substring(start, current);
-        tokens.add(new Token(type, text, literal, line));
-    }
-
     private char advance() {
         return source.charAt(current++);
     }
@@ -147,11 +138,18 @@ public class Scanner {
     private boolean match(char expected) {
         if (isAtEnd()) return false;
         if (source.charAt(current) != expected) return false;
-
         current++;
         return true;
     }
 
+    private void addToken(TokenType token) {
+        addToken(token, null);
+    }
+
+    private void addToken(TokenType token, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(token, text, literal, line));
+    }
 
     private char peek() {
         if (isAtEnd()) return '\0';
@@ -159,34 +157,27 @@ public class Scanner {
     }
 
     private char peekNext() {
+        // Peek at consequent next character without advancing
         if (current + 1 >= source.length()) return '\0';
         return source.charAt(current + 1);
     }
 
-    private boolean isAlpha(char c) {
-        return (c >= 'a' && c <= 'z') ||
-                (c >= 'A' && c <= 'Z') ||
-                (c == '_');
-    }
-
-    private boolean isAlphaNumeric(char c) {
-        return isAlpha(c) || isDigit(c);
-    }
-
     private void string() {
         while (peek() != '"' && !isAtEnd()) {
-            if (peek() != '\n') line++;
+            // Support multiline strings
+            if (peek() == '\n') line++;
             advance();
         }
 
         if (isAtEnd()) {
-            Rorix.error(line,"Unterminated String");
+            Rorix.error(line, "Unterminated String");
             return;
         }
 
-        // Closing
+        // Closing String
         advance();
 
+        // Trim the surrounding quotes -> Store the LITERAL value
         String value = source.substring(start + 1, current - 1);
         addToken(STRING, value);
     }
@@ -198,16 +189,26 @@ public class Scanner {
     private void number() {
         while (isDigit(peek())) advance();
 
-        // Fraction?
+        // Check for fraction
         if (peek() == '.' && isDigit(peekNext())) {
-            // Consume the .
+            // Skip over .
             advance();
 
             while (isDigit(peek())) advance();
         }
+        // Store every integer number as a double
         addToken(NUMBER,
                 Double.parseDouble(source.substring(start, current)));
     }
 
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c == '_');
+    }
 
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
 }
+
